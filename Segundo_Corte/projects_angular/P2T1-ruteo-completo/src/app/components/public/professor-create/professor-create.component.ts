287fba72873e1527fc804ecbf21fd487210a5c13
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-04-15 18:30:13 -0500

    Creación de Docentes

--- Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/components/public/professor-create/professor-create.component.ts
+++ Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/components/public/professor-create/professor-create.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { ARRAY_PROFESSOR } from 'src/app/mocks/professor-mock';
+import { Professor } from 'src/app/models/professor';
 
 @Component({
   selector: 'app-teacher-create',
@@ -7,9 +13,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorCreateComponent implements OnInit {
 
-  constructor() { }
+  public tempBase64: any;
+  public objProfessor: Professor;
+
+  constructor(public modalService:BsModalService, private toastr:ToastrService, private router:Router) {
+    this.objProfessor = new Professor(0,0,'', '', '');
+  }
 
   ngOnInit(): void {
   }
 
+  public selectPhoto(input: any): any{
+    if(!input.target.files[0] || input.target.files[0].length === 0) {
+      return;
+    }
+    const mimeType = input.target.files[0].type;
+    if(mimeType.match(/image\/*/) == null){
+      this.ToastrModal('Only <b>Images</b> are followed', 'Warning', 3);
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(input.target.files[0]);
+    reader.onload  = () => {
+      this.tempBase64 = reader.result;
+      this.objProfessor.photoBase64 = this.tempBase64;
+      this.objProfessor.photo = input.target.files[0].name;
+    };
+  }
+
+  public sendInfo(form:NgForm): boolean{
+    this.createProfessor();
+    this.objProfessor = new Professor(0,0,'','','');
+    this.ToastrModal('The professor has been <b>created</b> successfully', 'Success', 1);
+    this.router.navigate(['/professor/view']);
+    return true;
+  }
+
+  public createProfessor(): void{
+    this.objProfessor.cod = ARRAY_PROFESSOR.length+1;
+    ARRAY_PROFESSOR.push(this.objProfessor);
+  }
+
+
+  public ToastrModal(message:string, title:string, opcion:number):void{
+    const parameters = {
+        closeButton: true,
+        enableHtml: true,
+        progressBar: true,
+        positionClass: 'toast-top-right',
+        timeout: 8000
+      };
+      switch (opcion) {
+        case 1:
+          this.toastr.success(message, title, parameters);
+          break;
+        case 2:
+          this.toastr.info(message, title, parameters);
+          break;
+        case 3:
+          this.toastr.warning(message, title, parameters);
+          break;
+        case 4:
+          this.toastr.error(message, title, parameters);
+          break;
+
+        default:
+          break;
+      }
+  }
 }
--- Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/pipes/array.pipe.ts
+++ Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/pipes/array.pipe.ts
@@ -5,8 +5,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ArrayPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(externalArray: Array<any>, parameters: Array<string>): Array<any> {
+    const field = parameters[0];
+    const order = parameters[1];
+    return externalArray.sort(this.orderArray(field, order));
+  }
+
+  public orderArray(field: string, order = 'ASC'): any {
+    //? This function return a lambda function
+    return (arrayA: any, arrayB: any) => {
+      if (!arrayA.hasOwnProperty(field)) {
+        return 0;
+      }
+      let obj1 = arrayA[field];
+      let obj2 = arrayB[field];
+      const finalOrder = order.toUpperCase();
+      if (typeof arrayA[field] === 'string') {
+        obj1 = arrayA[field].toUpperCase();
+        obj2 = arrayB[field].toUpperCase();
+      }
+
+      let result = 0;
+      if (obj1 > obj2) { result = 1; }
+      else if (obj1 < obj2) { result - 1; }
+      return finalOrder === 'ASC' ? result : result * -1;
+    }
   }
 
 }
