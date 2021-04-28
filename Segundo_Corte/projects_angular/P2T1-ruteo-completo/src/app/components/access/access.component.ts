e45bfad627a54f1b9df6822a5e8b92b0d67f155e
carlos-paezf <carlos.paezf@usantoto.edu.co>
2021-04-27 20:45:16 -0500

    Login

--- Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/components/access/access.component.ts
+++ Segundo_Corte/projects_angular/P2T1-ruteo-completo/src/app/components/access/access.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-access',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccessComponent implements OnInit {
 
-  constructor() { }
+  public objUser: User;
+
+  constructor(public modalService:BsModalService, private toastr:ToastrService, private router:Router) {
+    this.objUser = new User(0,'','','','','');
+  }
 
   ngOnInit(): void {
+  }
+
+  public sendInfo(form: NgForm): boolean {
+    this.ToastrModal('<b>Sign In</b> sucessfully', 'Success', 1);
+    return true;
+  }
+
+  public ToastrModal(message: string, title: string, opcion: number) {
+    const parameters = {
+      closeButton: true,
+      enableHtml: true,
+      progressBar: true,
+      positionClass: 'toast-top-right',
+      timeout: 8000
+    };
+    switch (opcion) {
+      case 1:
+        this.toastr.success(message, title, parameters);
+        break;
+      case 2:
+        this.toastr.info(message, title, parameters);
+        break;
+      case 3:
+        this.toastr.warning(message, title, parameters);
+        break;
+      case 4:
+        this.toastr.error(message, title, parameters);
+        break;
+      default:
+        break;
+    }
   }
 
 }
